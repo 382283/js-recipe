81d8aaa33f90a6009347c29acf7b1afd0dc198c0
382283 <db23112@stumail.daido-it.ac.jp>
2025-05-18 17:20:18 +0900

    add dogapi

--- DOGAPI/main.js
+++ DOGAPI/main.js
@@ -1,4 +1,19 @@
 const imageElement = document.getElementById("dog-image")
+const button = document.createElement("div")
+button.className = "button"
+button.textContent = "犬の画像を更新"
+
+button.onclick = function () {
+  fetch("https:dog.ceo/api/breeds/image/random")
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      imageElement.src = data.message
+    })
+}
+
+document.body.appendChild(button)
 
 fetch("https:dog.ceo/api/breeds/image/random")
   .then((res) => {
